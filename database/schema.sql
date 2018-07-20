95ea743babfd2c259c8d44f4b0412a9cd5474ea3
lhuynh27 <leannehuynh27@gmail.com>
2018-07-20 16:41:30 -0700

    Fixed typo

--- database/schema.sql
+++ database/schema.sql
@@ -1,25 +1,23 @@
-DROP DATABASE IF EXISTS images;
-
-CREATE DATABASE images;
+CREATE DATABASE IF NOT EXISTS images;
 
 USE images;
 
 SET FOREIGN_KEY_CHECKS = 0;
 
-CREATE TABLE rooms (
+CREATE TABLE IF NOT EXISTS rooms (
 id int NOT NULL AUTO_INCREMENT,
 room_name varchar(50) NOT NULL,
-location varchar(50) NOT NULL,
+city varchar(50) NOT NULL,
 PRIMARY KEY (id)
 );
 
-CREATE TABLE images (
+CREATE TABLE IF NOT EXISTS images (
 id int NOT NULL AUTO_INCREMENT,
 url varchar(100) NOT NULL,
 description varchar(200),
 room_id int NOT NULL,
 PRIMARY KEY (id),
-FOREIGN KEY (room_id) REFERENCES room(id)
+FOREIGN KEY (room_id) REFERENCES rooms(id)
 );
 
 -- mysql -u root < schema.sql
