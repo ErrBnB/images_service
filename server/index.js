d3ebed5c3b3b6534117a19790557a4b71172bd53
lhuynh27 <leannehuynh27@gmail.com>
2018-07-20 19:49:45 -0700

    Implement server get request to db

--- server/index.js
+++ server/index.js
@@ -1,4 +1,17 @@
 const express = require('express')
 const app = express()
+const helpers = require('../database/index.js');
 
-app.listen(3000, () => console.log('Listening on port 3000'))
+app.listen(3000, () => console.log('Listening on port 3000'));
+
+app.get('/images', (req, res) => {
+  helpers.getImageInfo((err, data) => {
+    if(err){
+      console.log('Error Getting Image Info from DB:', err);
+      return err;
+    } else {
+      console.log('Able to Get Image Info from DB');
+      res.send(data);
+    }
+  })
+});
