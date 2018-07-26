e64b0e52c423f41eaf071dee258382a9dedbc723
lhuynh27 <leannehuynh27@gmail.com>
2018-07-26 00:49:33 -0700

    working ajax and click function

--- client/index.jsx
+++ client/index.jsx
@@ -1,20 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ListView from './ListView.jsx';
+import ImagePopUp from './ImagePopUp.jsx';
 import sampledata from './sampledata.js';
+import $ from 'jQuery'; 
 
 class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      images: sampledata.images
+      images: sampledata.images,
+      currentImage: ''
     };  
+    this.clickImage = this.clickImage.bind(this);
   }  
 
+  componentDidMount(){
+    const contextScope = this;
+    $.get("http://localhost:3000/images")
+      .done((data) => {
+        contextScope.setState({
+          images: data,
+          currentImage: data[0].url
+        })
+      });
+  }
+
+  clickImage(e){
+    this.setState ({
+      currentImage: e.target.src
+    })
+  }
+  
   render(){
     return(
       <div>
-        <ListView images={this.state.images} />
+        <img className="main-page-view" src={this.state.currentImage} />
+        <ListView clickImage={this.clickImage} images={this.state.images} />
       </div>
     )
   }     
